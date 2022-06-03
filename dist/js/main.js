8334b516ed3220cfce381ec282d78f3c34b43497
DalexisValencia <d.alexis.valencia@gmail.com>
2022-06-03 14:35:35 -0500

    Ajustando modal

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,20 @@
 (function ($) {
     $(document).ready(function () {
-       //jquery base
+        console.warn("kjsjsjks")
+        $('#scratchandwin').wScratchPad({
+            size: 70,
+            bg: 'https://dev-ruleta.pantheonsite.io/modules/custom/ab_raspaygana/dist/img/logo.png',
+            realtime: true,
+            fg: 'dist/img/slide2.jpg',
+            // 'cursor': 'url("http://jennamolby.com/scratch-and-win/images/coin1.png") 5 5, default',
+            scratchMove: function (e, percent) {
+                // $('#demo2-percent').html(percent);
+                  console.info("aqui", percent)
+                // if (percent > 70) {
+                //   this.clear();
+                // alert("Mostrar modal")
+                // }
+              }
+        });
     });
 })(jQuery);
