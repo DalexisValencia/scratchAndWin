097ede4aafb7760b5e62408e28f3bc03196a3436
DalexisValencia <d.alexis.valencia@gmail.com>
2022-06-07 09:40:53 -0500

    Ajustando evento scratchUp

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,30 +1,33 @@
 (function ($) {
-    $(document).ready(function () {
-        var showModal = false;
-        $('#scratchandwin').wScratchPad({
-            size: 70,
-            bg: 'dist/img/slide1.jpg',
-            realtime: true,
-            fg: 'dist/img/slide2.jpg',
-            // 'cursor': 'url("http://jennamolby.com/scratch-and-win/images/coin1.png") 5 5, default',
-            scratchMove: function (e, percent) {
-                // $('#demo2-percent').html(percent);
-                 // console.info("aqui", percent)
-                if (percent > 70 && !showModal) {
-                  showModal = true;
-                  this.clear();
-                  // alert("Mostrar modal")
-                  hiddeShowModal();
-                }
-              }
-        });
-
-        //show and hidde modal
-        function hiddeShowModal() {
-          $(".award.scratchAndWin").toggleClass("active");
-          $("body").toggleClass("no-scrol");
+  $(document).ready(function () {
+    var showModal = false;
+    $('#scratchandwin').wScratchPad({
+      size: 70,
+      bg: 'dist/img/slide1.jpg',
+      realtime: true,
+      fg: 'dist/img/slide2.jpg',
+      // 'cursor': 'url("http://jennamolby.com/scratch-and-win/images/coin1.png") 5 5, default',
+      scratchMove: function (e, percent) {
+        // $('#demo2-percent').html(percent);
+        // console.info("aqui", percent)
+        if (percent > 70 && !showModal) {
+          showModal = true;
+          this.clear();
+          // alert("Mostrar modal")
+          hiddeShowModal();
         }
-
-        $("#showModal").on("click", hiddeShowModal);
+      },
+      scratchUp: function(){
+        console.info("upp!!!")
+      }
     });
+
+    //show and hidde modal
+    function hiddeShowModal() {
+      $(".award.scratchAndWin").toggleClass("active");
+      $("body").toggleClass("no-scrol");
+    }
+
+    $("#showModal").on("click", hiddeShowModal);
+  });
 })(jQuery);
