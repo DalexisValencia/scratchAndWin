877a10449982880053967edd1023a87b64a28db7
DalexisValencia <d.alexis.valencia@gmail.com>
2022-06-14 08:55:18 -0500

    Ajustes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,42 @@
 (function ($) {
   $(document).ready(function () {
-    var showModal = false;
+    var showModal = false, randomCreated = false,
+    bgimg= jQuery('#urlimginferiorpremio').val(),
+    fgimg= jQuery('#urlimgsuperiorpremio').val();
+
     $('#scratchandwin').wScratchPad({
       size: 70,
-      bg: 'dist/img/slide1.jpg',
+      bg: 'dist/img/slide1.jpg', //bgimg
       realtime: true,
-      fg: 'dist/img/slide2.jpg',
+      fg: 'dist/img/slide2.jpg', //fgimg
       // 'cursor': 'url("http://jennamolby.com/scratch-and-win/images/coin1.png") 5 5, default',
       scratchMove: function (e, percent) {
-        // $('#demo2-percent').html(percent);
-        // console.info("aqui", percent)
         if (percent > 70 && !showModal) {
           showModal = true;
           this.clear();
-          // alert("Mostrar modal")
           hiddeShowModal();
         }
       },
-      scratchUp: function(){
-        console.info("upp!!!")
-      }
+      scratchDown: async function (e) {
+        nid = jQuery('#idtwig').val();
+        if (!randomCreated) {
+          randomCreated = true;
+          try {
+            await $.ajax({
+              url: Drupal.url("ab_raspaygana/premio"),
+              data: { id: nid },
+              type: "POST",
+              success: function (data) {
+                jQuery(".content-description").append(data.titulo)
+                jQuery(".imgwinner").attr("src", data.imagen)
+              }
+            });
+          } catch (err) {
+            jQuery(".content-description").append("Error interno")
+            jQuery(".award.scratchAndWin").addClass("error")
+          }
+        }
+      },
     });
 
     //show and hidde modal
@@ -28,6 +45,6 @@
       $("body").toggleClass("no-scrol");
     }
 
-    $("#showModal").on("click", hiddeShowModal);
+    // $("#showModal").on("click", hiddeShowModal);
   });
 })(jQuery);
